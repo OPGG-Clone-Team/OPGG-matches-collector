b870bf865a2fc64fe9901921cc1a47b0e58f17e7
so1omon <solmin3665@gmail.com>
2023-01-15 20:09:14 +0900

    [add] getSummoner def 추가

--- customRequest.py
+++ customRequest.py
@@ -2,56 +2,86 @@ import requests
 import os
 
 API_KEY = os.getenv("RIOT_API_KEY_1")
-# iter = 반복 횟수
 
-# tier에 존재하는 모든 소환사 정보 가져오기
-# 마그마챌 유저들만 request
-def league_exp_v4(tier, summoners, queue="RANKED_SOLO_5x5", division="I"):
+
+def league_v4(tier, queue="RANKED_SOLO_5x5"):
+  """
+  해당 tier에 존재하는 모든 소환사 정보 가져오기\n
+  50 requests every 10 seconds
+
+  Args:
+      tier (str, required): should be in ["challengerleagues", "grandmasterleagues","masterleagues"].
+      queue (str, optional): 조회할 큐 선택, Defaults to "RANKED_SOLO_5x5".
+
+  Returns:
+      [LeagueListDTO]: {
+        freshBlood (boolean) : 아직 뭔지모름,
+        wins (int),
+        summonerName (string),
+        miniSeries (MiniSeriesDTO),
+        inactive (boolean) : 아직 뭔지모름,
+        veteran	(boolean) : 아직 뭔지모름,
+        hotStreak	(boolean) : 아직 뭔지모름,	
+        rank (string): 세부 티어 (ex: Diamond 1 -> rank : "I"),
+        leaguePoints (int) 점수,	
+        losses (int),
+        summonerId (string),
+      }
+  """
+  if tier not in ["challengerleagues", "grandmasterleagues","masterleagues"]:
+    return []
   
-  if tier not in ["CHALLENGER", "GRANDMASTER","MASTER"]:
-    return None
+  summoners=[]
   
-  for i in range(1, iter+1):
-    url = f"https://kr.api.riotgames.com/lol/league-exp/v4/entries/{queue}/{tier}/{division}?page={i}"
+  url = f"https://kr.api.riotgames.com/lol/league/v4/{tier}/by-queue/{queue}"
 
-    # 추후 logging 적용
-    print(f'다음으로 request : {url}')
+  # 추후 logging 적용
+  print(f'다음으로 request : {url}')
 
-    results = requests.get(url, headers={"X-Riot-Token":API_KEY}).json() ## list type expected
-    
-    if not results: # fetching한 데이터가 비어 있으면 함수 즉시 종료
-      return summoners
-    
-    # 만약 list데이터로 넘어오지 않는다면 (ERROR 발생) response 출력 후 함수 종료
-    if not isinstance(results, list): 
-      print(results)
-      return summoners
-    
-    summoners.extend(results)
-
+  result = requests.get(url, headers={"X-Riot-Token":API_KEY}).json() ## list type expected
+  
+  entries = result["entries"]
+  
+  if not entries: # fetching한 데이터가 비어 있으면 함수 즉시 종료
+    return summoners
+  
+  # 만약 list데이터로 넘어오지 않는다면 (ERROR 발생) response 출력 후 함수 종료
+  if not isinstance(entries, list): 
+    print(result)
+    return summoners
+  
+  summoners.extend(entries)
   return summoners
 
-def league_exp_v4(tier, summoners, queue="RANKED_SOLO_5x5", division="I"):
+def getSummoner(id):
+  """
+  summonerId로 Summoner 정보 가져오기\n
+  1600 requests every 1 minutes\n
+  경고 - Consistently looking up summoner ids that don't exist will result in a blacklist.\n
   
-  if tier not in ["CHALLENGER", "GRANDMASTER","MASTER"]:
-    return None
+  Args:
+      id (str): summonerId
+
+  Returns:
+      Summoner: {
+        "id": 소환사 ID,
+        "accountId": 소환사 계정 ID,
+        "puuid": 소환사 PUUID,
+        "name": 소환사명,
+        "profileIconId": 프로필 아이콘 id,
+        "revisionDate": 소환사 정보 최종 수정일,
+        "summonerLevel": 소환사 레벨,
+        "queryAllowTime": 해당 소환사를 query 가능한 시간, default : null
+      }
+  """
   
-  for i in range(1, iter+1):
-    url = f"https://kr.api.riotgames.com/lol/league-exp/v4/entries/{queue}/{tier}/{division}?page={i}"
+  url = f"https://kr.api.riotgames.com/lol/summoner/v4/summoners/{id}"
 
-    # 추후 logging 적용
-    print(f'다음으로 request : {url}')
-
-    results = requests.get(url, headers={"X-Riot-Token":API_KEY}).json() ## list type expected
-    
-    if not results: # fetching한 데이터가 비어 있으면 함수 즉시 종료
-      return summoners
-    
-    # 만약 list데이터로 넘어오지 않는다면 (ERROR 발생) response 출력 후 함수 종료
-    if not isinstance(results, list): 
-      print(results)
-      return summoners
-    
-    summoners.extend(results)
-
-  return summoners
+  # 추후 logging 적용
+  print(f'다음으로 request : {url}')
+  result = requests.get(url, headers={"X-Riot-Token":API_KEY}).json()
+  
+  if "id" not in result:
+    return {}
+  
+  return result
--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import dotenv
 dotenv.load_dotenv(override=True) # .env file 로드
 
-from customRequest import league_exp_v4
+from customRequest import league_v4, getSummoner
 from mongo import mongoClient
 
 db=mongoClient.LEAGUEDATA
 
 summoners=[]
 
-summoners.extend(league_exp_v4("CHALLENGER", summoners))
-summoners.extend(league_exp_v4("GRANDMASTER", summoners))
-summoners.extend(league_exp_v4("MASTER", summoners))
+summoners.extend(league_v4("challengerleagues"))
+summoners.extend(league_v4("grandmasterleagues"))
+summoners.extend(league_v4("masterleagues"))
 
 # 추후 로깅 적용
-print('--------- 마스터 이상 리그 id insert ---------')
+print('--------- 마스터 이상 소환사 정보 insert ---------')
 
+# 임시 request 횟수 count 변수
+request_count = 0 
 # 추후 예외 처리
 for summoner in summoners:
-  db.league_entries.update_one({'summonerId':summoner["summonerId"]}, {"$set":summoner}, True)
-
-
-
+  request_count+=1
+  summonerId = summoner["summonerId"]
+  filter = {'summonerId':summonerId}
+  
+  # 추후 예외처리로 삭제
+  if not summonerId:
+    continue
+  
+  db.league_entries.update_one(filter, {"$set":summoner}, True) # Upsert:True
+  db.summoners.update_one(filter, {"$set":getSummoner(summonerId)}, True) #Upsert:True
 
