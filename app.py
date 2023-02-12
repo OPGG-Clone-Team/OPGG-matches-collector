f4e160da22f9fd22f5e8545264ddce6066978557
so1omon <solmin3665@gmail.com>
2023-02-12 19:36:24 +0900

    [fix] app.py FLASK_DEBUG remove

--- app.py
+++ app.py
@@ -167,6 +167,5 @@ def create_app():
 if __name__ == "__main__":
   app.run(
     host = app.config["FLASK_HOST"], 
-    port=app.config["FLASK_PORT"], 
-    debug=app.config["FLASK_DEBUG"])
+    port=app.config["FLASK_PORT"])
   
